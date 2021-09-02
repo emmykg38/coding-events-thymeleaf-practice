5b885e36ae91a7c6c090cf703659ddbd91c23f20
millylane003 <emilykgerst19@gmail.com>
2021-09-02 12:44:02 -0500

    fragments branch completed

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -4,7 +4,9 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,14 +16,10 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    private static List<String> events = new ArrayList<>();
+
     @GetMapping
     public String showEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("First Fridays at California Co-op");
-        events.add("Saturday Farmer's Market");
-        events.add("Layla's football game");
-        events.add("Festival of Nations");
-        events.add("Japanese Festival at MoBot");
         model.addAttribute("events", events);
         return "events/index";
     }
@@ -33,5 +31,12 @@ public class EventController {
         return "events/create";
     }
 
+    //lives at /events/create
+    //eventName (parameter) is the value of the name attribute for the text input box
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName){
+        events.add(eventName);
+        return "redirect:";
+    }
 
 }
